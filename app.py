36686eed52101fa6a198b86815b9a4e274dbf7f2
abhikp98 <97169926+abhikp98@users.noreply.github.com>
2022-10-16 01:55:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 @app.route('/test')
 def val():
     link = str(request.args.get('link'))
-    key, vall, = [], []
+    key, vall, lis = [], [], []
     ttl = YouTube(link).title
     tnu = YouTube(link).thumbnail_url
     for i in YouTube(link).streams.filter(type="video").order_by("resolution"):
         if i.resolution not in key:
             key.append(i.resolution)
             vall.append(i.url)
-    ab = dict(zip(key, vall))
-    ab["title"] = ttl
-    ab["thumbnail"] = tnu
-    ab["qualitylist"] = key
-    return ab
+    for i, j in zip(range(len(key)), range(len(vall))):
+        a = {"name": key[i], "link": vall[j]}
+        lis.append(a)
+    last = {"data": lis, "title": ttl, "qualitylist": key, "thumbnail": tnu}
+    return last
 
 
 if __name__ == "__main__":
